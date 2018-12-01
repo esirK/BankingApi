243a62818214265ba2a5a77d882dbdf326de6ca9
esir <esirkings@gmail.com>
2018-12-01 05:51:09 +0300

    Creates a custom manager with custom create_user and create_superuser commands

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,37 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
+class CustomManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, **fields):
+        email = fields.pop('email')
+        password = fields.get('password')
+
+        if not email:
+            raise ValueError("Email address is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, **fields):
+        fields.setdefault('is_staff', True)
+        fields.setdefault('is_superuser', True)
+        fields.setdefault('is_active', True)
+
+        self._create_user(**fields)
+
+    def create_user(self, **fields):
+        fields.setdefault('is_staff', False)
+        fields.setdefault('is_superuser', False)
+
+        return self._create_user(**fields)
+
 class User(AbstractUser):
+    objects = CustomManager()
     email = models.EmailField(max_length=50, unique=True)
 
     is_active = models.BooleanField(
