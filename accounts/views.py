8e11db6f0abcbdf286ff6f433b2da283be38fd2c
esir <esirkings@gmail.com>
2018-12-04 05:12:09 +0300

    Adds User login endpoint.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
-from accounts.serializers import TellerSerializer, CustomerSerializer
+from accounts.serializers import (TellerSerializer,
+                                  CustomerSerializer,
+                                  LoginSerializer)
 
 User = get_user_model()
 
@@ -15,4 +18,17 @@ class UserRegistrationAPIView(generics.CreateAPIView):
             return TellerSerializer
         if self.request.user.is_teller:
             return CustomerSerializer
+
+
+class UserLoginAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
 
